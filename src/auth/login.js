407df1ad8520a3dd768b5005c3dc964c1647d44f
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-10-28 14:20:10 +0100

    update

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { loginEndpoint } from '../spotify';
 import './login.css';
-import logo from '../images/winamp-logo.webp';
+import logo from '../images/logo2.png';
 
 export default function Login() {
     return (
