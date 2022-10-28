635fbd0b8e3dd36334542d8a160e23efe597b76d
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-10-28 14:14:11 +0100

    update

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { loginEndpoint } from '../spotify';
 import './login.css';
+import logo from '../images/winamp-logo.webp';
 
 export default function Login() {
     return (
         <div className='login-page'>
-            <img src="https://imgs.search.brave.com/bAQcuhLFXbh10wzQWvrRp-9hBNn3j4KWQP68-zfZXBM/rs:fit:1200:1200:1/g:ce/aHR0cDovLzEwMDBs/b2dvcy5uZXQvd3At/Y29udGVudC91cGxv/YWRzLzIwMTcvMDgv/U3BvdGlmeS1Mb2dv/LnBuZw" 
+            <img src={logo} 
             alt='logo-spotify' className='logo' />
             <a href={loginEndpoint}>
                 <div className='login-btn'>LOG IN</div>
