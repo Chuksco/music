5e33750db0d97847813cf5689b8ba0691d96a2f6
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-11-01 11:50:19 +0100

    update

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import APIKit from '../../spotify'
 
 export default function Library() {
-  return <div className='screen-container'></div>
+
+  const [playlists, setPlaylists] = useState(null);
+
+  APIKit.get("me/playlists").then(function(response) {
+    console.log(response.data);
+    console.log(response.data.items);
+  })
+  return <div className='screen-container'>
+    {playlists?.map((playlist) => {
+      <div></div>;
+    })}
+  </div>
 }
