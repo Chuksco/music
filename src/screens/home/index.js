ec095a64347758577a79b8abdf23a1b17d267d7d
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-11-04 13:17:34 +0100

    update

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,36 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "../../components/sidebar";
-import { setClientToken } from "../../spotify";
-import Login from "../auth/login";
-import Favorites from "../favorites";
-import Feed from "../feed";
-import Library from "../library";
-import Player from "../player";
-import Trending from "../trending";
+import Player from "../player/player";
+import Library from "../library/index";
+import Feed from "../feed/feed";
+import Trending from "../trending/trending"
+import Favorites from "../favorites/favorites"
 import "./home.css";
 
 export default function Home() {
-  const [token, setToken] = useState("");
-
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    const hash = window.location.hash;
-    window.location.hash = "";
-    if (!token && hash) {
-      const _token = hash.split("&")[0].split("=")[1];
-      window.localStorage.setItem("token", _token);
-      setToken(_token);
-      setClientToken(_token);
-    } else {
-      setToken(token);
-      setClientToken(token);
-    }
-  }, []);
-
-  return !token ? (
-    <Login />
-  ) : (
+  return  (
     <Router>
       <div className="main-body">
         <Sidebar />
@@ -38,7 +17,7 @@ export default function Home() {
           <Route path="/" element={<Library />} />
           <Route path="/feed" element={<Feed />} />
           <Route path="/trending" element={<Trending />} />
-          <Route path="/player" element={<Player />} />
+          <Route path="/player" element={<Player />} /> 
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </div>
